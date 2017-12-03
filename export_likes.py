d39abff9e8e10d477c44c283cd5e9cf1e7f4a4ff
渡部真伍 <s1170087@gmail.com>
2017-12-03 21:48:10 +0900

    modify: to delete max_id keyword parameter

--- export_likes.py
+++ export_likes.py
@@ -4,7 +4,7 @@ from twitter_exporter import all_likes
 GET_TWEET_URL = 'https://twitter.com/{}/status/{}'
 
 def main() -> None:
-    for like in all_likes(sleep_time=20, max_id='358878880335011840'):
+    for like in all_likes(sleep_time=20):
         tweet_url = GET_TWEET_URL.format(like['user']['screen_name'], like['id'])
         print(tweet_url)
         sys.stdout.flush()
